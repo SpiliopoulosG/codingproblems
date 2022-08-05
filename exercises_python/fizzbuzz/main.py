bc7f4eb90cc17bd38d236736e702c5472eeb2837
George Spiliopoulos <george.spiliopoulos@upstreamsystems.com>
2022-08-05 13:56:04 +0300

    Added small improvements and fizzbuzz and maxChar

---
+++ exercises_python/fizzbuzz/main.py
@@ -0,0 +1,11 @@
+#  --- Directions
+#  Return a number from 1 to n. But for multiples of three return
+#  “Fizz” instead of the number and for the multiples
+#  of five return “Buzz”. For numbers which are multiples
+#  of both three and five return “FizzBuzz”.
+#  --- Example
+#  fizzBuzz(5)
+#  buzz
+
+def fizzBuzz(num):
+    pass
